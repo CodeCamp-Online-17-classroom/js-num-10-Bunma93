b9ecbcb74da5394310f78b1680e7263328197bf2
Bunma93 <bunma93@gmail.com>
2024-07-08 13:30:14 +0700

    Js-num10

--- app.js
+++ app.js
@@ -1,1 +1,8 @@
 // write code here
+function degreesToRadians(degrees) {
+    return degrees * (Math.PI / 180);
+}
+
+let degrees = prompt("มุม(องศา)");
+let radians = degreesToRadians(degrees).toFixed(2);
+alert(`${degrees} องศา = ประมาณ ${radians} เรเดียน`);
